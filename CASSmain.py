e46735fe0bc8311466ce56ce1292e35db6c5f286
beachybum28 <beachybum28@gmail.com>
2013-07-25 17:06:27 -0400

    Update CASSmain.py

--- CASSmain.py
+++ CASSmain.py
@@ -1,28 +1,26 @@
-import CASSparser
-import CASSuserInputConverter
-import CASSprocessor
+import CASSparser, CASSuserInputConverter, CASSprocessor, CASSoutput
+import sys
 
 def mainCASS():
     print "****************************"
     print "******Welcome to CASS!******"
     print "****************************"
-    print "-This stochastic simulator is"
-    print "useful for modeling"
-    print "biochemical networks."
-
-    print "Make sure your file is in the correct format (See Documentation)\n and make sure it is in the current working directory\n"
+    print "-This stochastic simulator is useful for modeling biochemical networks."
+    print "-Make sure your file is in the correct format (See Documentation)"
+    print "and verify that the file is in the current working directory\n"
 
     loop = True
     while(loop):
         try:
             print "Please enter your text file name (do not include .txt): ",
             fileName = str(raw_input())
+            fooFile = open(fileName+".txt")
             loop=False
         except IOError:
             print "Error - File does not exist"
-        
+
+    #sets inputs to temporary variables for organization
     elements = CASSuserInputConverter.inputReactions(fileName)
-    #print elements
     rxnsAndMolCounts=elements[0]
     duration=elements[1]
     maxIterations=elements[2]
@@ -31,10 +29,10 @@ def mainCASS():
     inputName=elements[5]
 
     EqnsNmolCounts = CASSparser.parseText(rxnsAndMolCounts)
-    print EqnsNmolCounts
     tupleInputs = EqnsNmolCounts[0]
     molCounts = EqnsNmolCounts[1]
 
+    #Calls processor
     CASSprocessor.updateAll(tupleInputs, molCounts, duration, maxIterations, outputFreq, molVSList, inputName)
 
 mainCASS()
