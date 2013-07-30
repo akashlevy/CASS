0d16781de039d99869c5ade7e44fd56a199176ea
Akash Levy <akashlevy@gmail.com>
2013-07-30 14:59:15 -0400

    Commented CASSprocessor.py

--- CASSprocessor.py
+++ CASSprocessor.py
@@ -1,7 +1,7 @@
-import math, os, numpy, pylab, sys, datetime, time as tm, random as rng
+import math, os, numpy, pylab, datetime, time as tm, random as rng
 import CASSoutput
 
-#Data Structure
+#Input Data Structure
     #"tupleInputs" = list of Tuples (each tuple represents a reaction)
         #0-"kVal"=float K-value of reaction
         #1-"rxns"=dictionary of ("Molecule Name":difference in coefficients)
@@ -19,26 +19,29 @@ class ProcessingError(Exception):
         print string
         print
         print "The program will now exit."
-        sys.exit(1)
+        exit(1)
 
 def updateAll(tupleInputs, molCounts, maxTime, maxIterations, outputFreq, molVSList, seed=1234213):
+    #Initialize variables
     rng.seed(seed)
-    time       = 0.0
-    iteration  = 0
+    time = 0.0
+    iteration = 0
 
     #Prints parameters
-    print "Maximum Time: ",maxTime
+    print "Maximum Time: ", maxTime
     print "Maximum number of Iterations: ", maxIterations
     print "Output Frequency: ", outputFreq
     print "Molecules: ",
     for key in molCounts.keys():
         print key,
     print "\nPlots: ",
-    if molVSList !=None:
+    if molVSList != None:
         for vs in molVSList:
             if((vs[0] not in molCounts and "time" not in vs[0].lower()) or (vs[1] not in molCounts and "time" not in vs[1].lower())):
                     raise ProcessingError("\nError - Plotting variables (%s) that do not exist"%("%s vs. %s"%(vs[0],vs[1])))
             print "%s vs. %s,"%(vs[0],vs[1]),
+
+    #Processing message
     print "\n------------------"
     print "Processing..."
     print "------------------"
@@ -53,7 +56,11 @@ def updateAll(tupleInputs, molCounts, maxTime, maxIterations, outputFreq, molVSL
         #1. The simulated time reaches the user-input maximum time
         #2. The number of iterations reaches the user-input maximum number of iterations
         #3. All reaction propensities reach 0 (i.e. all reactions ran to completion or insufficient reactants remain for any new products)
+
+    #Start timer
     start_time = tm.time()
+
+    #Gillespie Algorithm
     while(time < maxTime and iteration < maxIterations):
         props = [] #list of propensity values for each reaction
         
@@ -62,7 +69,7 @@ def updateAll(tupleInputs, molCounts, maxTime, maxIterations, outputFreq, molVSL
             props.append(prop)
             
         sump = sum(props)
-        if(sump==0):
+        if (sump==0):
             print "All reaction propensities have reached 0. The system has reached equilibrium"
             break
         rand_1 = rng.random()
@@ -75,29 +82,35 @@ def updateAll(tupleInputs, molCounts, maxTime, maxIterations, outputFreq, molVSL
         while(threshold > summation):
             summation += props[count]
             count += 1
-        rxnChoice = tupleInputs[count-1][2] #Dictionary of change in coefficients (Key=Molecule Name)
+        rxnChoice = tupleInputs[count-1][2] #dictionary of change in coefficients (Key=Molecule Name)
         molCounts = reactionUpdater(rxnChoice,molCounts)
         write_data_to_output(fileHandles, time, molCounts)
         if (iteration % outputFreq == 0):
             print "iteration %d   time %5.4g" % (iteration, time)   
         iteration += 1
+
+    #Close output file handles
     close_output_files(fileHandles)
+
+    #Stop timer
     end_time = tm.time()
-    if(molVSList!=None):
-        print "Time Elapsed = " + str(end_time - start_time)
-        return CASSoutput.graphResults(fileHandles,molCounts,molVSList,suffix)
+
+    #Display time elapsed
+    #if(molVSList != None):
+    #    print "Time Elapsed = " + str(end_time - start_time)
+    #    return CASSoutput.graphResults(fileHandles,molCounts,molVSList,suffix)
 
 
-#takes one tuple and returns propensity based on Gillespie Algorithm   
+#Takes one tuple and returns propensity based on Gillespie Algorithm   
 def computePropensity(tupleInput, molCounts):
-    kVal=tupleInput[0]# k-value
-    coeffs=tupleInput[1] #Dictionary of coefficients for each molecule reactants only(key=Molecule Name)
-    propProduct=kVal # the multiplicative-accumulator is initialized to the reaction k-value
+    kVal=tupleInput[0] #k-value
+    coeffs=tupleInput[1] #dictionary of coefficients for each molecule reactants only(key=Molecule Name)
+    propProduct=kVal #multiplicative-accumulator is initialized to the reaction k-value
     for key in coeffs:
         while True:
             try:
-                if(coeffs[key]>0):
-                    # multiplied by permutation of molCounts for each reactant molecule divided by the coefficient
+                if (coeffs[key] > 0):
+                    #Multiplied by permutation of molCounts for each reactant molecule divided by the coefficient
                     #(e.g. The propensity of reactants 3A + 2B is: (1/3*(N_A)*(N_A-1)*(N_A-2))*(1/2*(N_B)*(N_B-1)))
                     propProduct *= (1.0*calcNPR(molCounts[key],coeffs[key])*(1.0/(coeffs[key])))
                 break
@@ -105,8 +118,8 @@ def computePropensity(tupleInput, molCounts):
                 raise ProcessingError("ERROR - %s is not in the molecule list"%(key))
     return propProduct
 
-#Takes two integers n and r, and returns nPr (i.e. P(n,r))
-#Permutation function which returns 0 if r>n
+#Takes two integers n and r, and returns the permutation: nPr (i.e. P(n,r))
+#Returns 0 if r > n
 def calcNPR(n,r):
     product=1
     while(r>0):
@@ -121,7 +134,6 @@ def reactionUpdater(rxn,molCounts):
     for key in rxn:
         if(key in molCounts.keys()):
             molCounts[key]+=rxn[key] #corresponds molecules based on key name
-            #assert(molCounts[key]>0), 'Molecule %s has ran out'%(key)
         else:
             raise ProcessingError("ERROR - %s Molecule not Found"%(key))
     return molCounts
@@ -134,7 +146,8 @@ def open_output_files(molCounts, suffix):
         path = str(os.getcwd()) + "\\Results_" + suffix
         name =("%s.dat"%(key))
         os.path.join(path,name)
-        files.append(open(os.path.join(path,name),"w"))#Add folder for results
+        #Add folder for results
+        files.append(open(os.path.join(path,name),"w"))
     return files
 
 #Writes title headers on each output file "Time" and "Molecule Count"
@@ -144,7 +157,7 @@ def write_titles_to_outputFiles(fileHandles, molCounts):
         fileHandles[i].write("Time\t  Molecule Count\n")
         i+=1
 
-#writes data for all files
+#Writes data for all files
 def write_data_to_output(fileHandles, time, molCounts):
     #This function writes a (time, data) pair to the corresponding output file. concentrations not molecule counts.
     i=0
@@ -152,8 +165,7 @@ def write_data_to_output(fileHandles, time, molCounts):
         fileHandles[i].write("%5.4e %8.7f\n" %(time, molCounts[key]))
         i+=1
 
-#closes fileHandles to avoid problems
+#Closes fileHandles to avoid errors
 def close_output_files(fileHandles):
     for i in range(0,len(fileHandles)):
         fileHandles[i].close()
-
